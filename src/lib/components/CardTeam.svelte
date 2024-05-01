5ee95f1990dc46282b6c5d7487306b7d24f2f708
Danial <danial.sh7@mail.ru>
2024-05-01 20:54:46 +0600

    feat: team card component

---
+++ src/lib/components/CardTeam.svelte
@@ -0,0 +1,105 @@
+<script>
+    export let cardInfo = {
+        imgUrl: undefined,
+        title: undefined,
+        content: undefined,
+        people: -1,
+        participate: false,
+    };
+</script>
+
+<div class="card-container">
+    <div
+        class="card-header"
+        style:background-image={`url(${cardInfo.imgUrl})`}
+    ></div>
+    <div class="card-body">
+        <div class="title">
+            Команда {cardInfo.title}
+        </div>
+        <div class="content">
+            {cardInfo.content}
+        </div>
+        <div class="people">
+            <img src="./src/assets/imgs/people_number_icon.svg" alt="icon">{cardInfo.people}
+        </div>
+    </div>
+    <div class="card-footer">
+        {#if (cardInfo.participate = true)}
+            <button class="view">Текущая команда</button>
+        {:else}
+            <button class="join">Вступить</button>
+        {/if}
+    </div>
+</div>
+
+<style>
+    .card-container {
+        height: 280px;
+        width: 256px;
+        border-radius: 20px;
+        background: #f6f6f6;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .card-header {
+        background-color: rgb(0, 170, 170);
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        flex-grow: 5;
+        border-radius: 20px 20px 0px 0px;
+    }
+
+    .card-body {
+        flex-grow: 2;
+        padding: 8px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    }
+
+    .card-footer {
+        flex-grow: 1;
+    }
+
+    .title {
+        font-size: 1.3rem;
+        font-weight: bold;
+    }
+
+    .content {
+        font-size: 1rem;
+    }
+
+    .people {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+    }
+
+    button {
+        width: 100%;
+        height: 100%;
+        border: none;
+        border-radius: 0px 0px 20px 20px;
+        font-size: 1.3rem;
+        font-weight: bold;
+        color: white;
+        transition: 0.2s ease;
+    }
+
+    button:hover {
+        cursor: pointer;
+        opacity: 0.9;
+    }
+
+    .join {
+        background-color: #ff9d00;
+    }
+
+    .view {
+        background-color: #04d200;
+    }
+</style>
