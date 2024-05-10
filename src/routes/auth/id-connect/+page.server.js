b24db9e33c79d0e0b2f0cb07da2ce7e849a8148d
Zhansen <supersegizbay@gmail.com>
2024-05-10 21:06:21 +0600

    feat: microsoft-entry link

--- src/routes/auth/id-connect/+page.server.js
+++ src/routes/auth/id-connect/+page.server.js
@@ -1,12 +1,3 @@
-import { redirect } from '@sveltejs/kit';
-
 export const load = async ({ parent }) => {
 	await parent();
 }
-
-export const actions = {
-	default: ({ cookies, url }) => {
-		cookies.set('logged_in', 'true', { path: '/' });
-		throw redirect(303, url.searchParams.get('redirectTo') ?? '/');
-	}
-};
--- src/routes/auth/id-connect/+page.svelte
+++ src/routes/auth/id-connect/+page.svelte
@@ -6,6 +6,6 @@
     <h1>Добро пожаловать!</h1>
     <p>Что бы войти вы должны являться студентом АИТУ!</p>
     <Button>
-        <a href="register">OpenID connect</a>
+        <a href="https://login.microsoftonline.com/158f15f3-83e0-4906-824c-69bdc50d9d61/oauth2/v2.0/authorize?client_id=8e43eadc-4f43-4434-b074-7dd9b8d46468&response_type=code&redirect_uri=http%3A%2F%2Flocalhost:5173%2Fauth%2Fregister&response_mode=query&scope=openid&state=12345">OpenID connect</a>
     </Button>
 </div>
