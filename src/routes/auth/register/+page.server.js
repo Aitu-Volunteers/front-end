d8dc102c9a8fddb4835e006976dfc56090cad8d6
Zhansen <supersegizbay@gmail.com>
2024-05-16 20:55:06 +0600

    feat: Entra code extract

--- src/routes/auth/register/+page.server.js
+++ src/routes/auth/register/+page.server.js
@@ -1,3 +1,11 @@
-export const load = async ({ parent }) => {
+import { redirect } from '@sveltejs/kit';
+import { API_HOST_URL } from '$env/static/private';
+
+
+export const load = async ({ parent, fetch, url }) => {
     await parent();
+
+    let code = url.searchParams.get('code');
+    console.log(code);
+    // const res = await fetch(`${API_HOST_URL}/api/v1/auth/login?code=${code}`);
 }
