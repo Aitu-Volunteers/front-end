89b8151f66a61f48537acf3897c5bbabc8f4749b
Zhansen <supersegizbay@gmail.com>
2024-05-28 14:05:34 +0600

    feat: add handle hook and stores

---
+++ src/lib/stores/writables.js
@@ -0,0 +1,22 @@
+import { browser } from "$app/environment";
+import { writable } from "svelte/store";
+
+// Custom theme tracking store
+function themeTracker(defaultTheme) {
+    const { subscribe, update } = writable(browser && localStorage.getItem("theme") || defaultTheme);
+    
+    function toggleTheme() {
+        update((theme) => {
+            const upd = theme === "dark" ? "light" : "dark";
+            localStorage.setItem("theme", upd);
+            return upd;
+        });
+    }
+
+    return {
+        subscribe,
+        toggleTheme,
+    }
+}
+
+export const theme = themeTracker("light");
